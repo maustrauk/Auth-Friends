7b33fa27179f564975c618046e8cf90a220858ee
Iaroslav Mokroguz <maustrauk@gmail.com>
2020-12-15 22:28:59 -0500

    Task 2: Adding Friend Delete

--- friends/src/components/Friend.js
+++ friends/src/components/Friend.js
@@ -2,15 +2,19 @@ import React from 'react';
 
 const Friend = props => {
 
-    const {friend} = props;
+    const {friend, deleteFriend, updateFriend} = props;
+
+    const onDelete = () => {
+        deleteFriend(friend.id);
+    }
 
     return(<div className="card mr-3 mb-3">
         <div className="card-body">
             <h5 className="card-title">{friend.name}</h5>
             <h6 className="card-subtitle mb-2 text-muted">Age: {friend.age}</h6>
             <p className="card-text">E-mail: {friend.email}</p>
-            <button type="button" className="btn btn-success mr-3">Update</button>
-            <button type="button" className="btn btn-danger">Delete</button>
+            <button type="button" className="btn btn-success mr-3" onClick={updateFriend}>Update</button>
+            <button type="button" className="btn btn-danger" onClick={onDelete}>Delete</button>
         </div>
     </div>)
 }
--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -28,12 +28,27 @@ const FriendsList = props => {
         props.history.push('/newFriend');
     }
 
+    const deleteFriend = (id) => {
+        axiosWithAuth()
+        .delete(`friends/${id}`)
+        .then(res => {
+            getFriends(res.data);
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    }
+
+    const updateFriend = () => {
+
+    }
+
     return (<div className="friends-list container-fluid text-center">
         <h1 className="display-1 mt-3"> Friends List</h1>
         <button type="button" className="btn btn-primary" onClick={clickHandler}>Add Friend</button>
         <div className="container-fluid row m-3 ">
             {friends.map(friend => (
-                <Friend key={friend.id} friend={friend} />
+                <Friend key={friend.id} friend={friend} deleteFriend={deleteFriend} updateFriend={updateFriend}/>
             ))}
         </div>
     </div>)
